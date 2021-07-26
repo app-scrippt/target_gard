d8e34c27841651b4d58a80bf76683631e06202bb
tasneem <masa1234>
2021-07-26 14:46:04 +0200

    initial module

---
+++ target_gard/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Target Gard Module',
+    'category': 'Website',
+    'summary': 'Target Gard Mobile Application',
+    'description': "",
+    'depends': ['hr','sale','contacts','account'],
+    'data': [
+        # 'security/ir.model.access.csv',
+
+    ],
+    'demo': [
+    ],
+    'installable': True,
+}
---
+++ target_gard/controllers/__init__.py
@@ -0,0 +1,7 @@
+
+
+from . import auth
+from . import customers
+from . import products
+from . import sales_person
+from . import orders
